7e6476a9acfa9a6ecd60799ed18051f1a1b1c562
Tyrone Kedidimetse Rantlhoatlhoa <154645924+KRWAKANDA@users.noreply.github.com>
2025-10-24 10:52:48 +0200

    Add files via upload

---
+++ first_pyhon_program.py
@@ -0,0 +1,17 @@
+name = "Ivy Mpho"
+age = 22
+is_developer = True
+goal = "To be a full stack developer"
+height = 1.65
+skills = ["Python", "HTML", "CSS", "Javascript", "Python"]
+hobbies = ("coding", "design" ,"basketball" )
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I am a developer?", is_developer)
+print("My height is",height)
+print("My goal is", goal)
+print("My hobbies are", hobbies)
+print("My skills are", skills)
+
+print("here are a few things about me")
